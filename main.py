c9c8c54b1b39072b01c0036e43ca335a2efd4e2c
edusalagran <eduard.ferragut@oceandata.earth>
2023-12-21 12:28:10 +0100

    Add parallelism

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import multiprocessing
 import random
 from datetime import datetime
 from pathlib import Path
 from typing import List, Tuple
+
+from tqdm import tqdm
 
 from src.elements.patient import Patient
 from src.heuristics import EvolutionaryAlgorithm, HeuristicBase, HeuristicGenerator, PredefinedOrder
@@ -11,6 +14,31 @@ from src.tester import tester
 
 FOLDER_INSTANCES = Path.cwd() / "data" / "Exemplars" / "data"
 FOLDER_RESULTS = Path.cwd() / "data" / "Exemplars" / "solutions"
+
+
+def get_best_order_par(heuristic_path: Tuple[HeuristicBase, Path]) -> Tuple[List[Patient], Solution]:
+    heuristic, path_input = heuristic_path
+    instance = get_instance(path_input)
+    solution = Solution(instance)
+    solution_list = solution.find_solution(heuristic)
+    return solution_list, solution
+
+
+def run_parallel(
+    heuristics: List[HeuristicBase], result: Result, path_input: Path, cpu_time: float
+) -> List[Tuple[List[Patient], float]]:
+    solutions: List[Tuple[List[Patient], Solution]] = []
+    heuristics_to_process = [(heuristic, path_input) for heuristic in heuristics]
+    with multiprocessing.Pool() as pool:
+        solutions = pool.map(get_best_order_par, heuristics_to_process)
+
+    returning_value: List[Tuple[List[Patient], float]] = []
+    for list_patients, solution in solutions:
+        returning_value.append((list_patients, solution.value()))
+        if result.best_sol is None or result.best_sol.value() < solution.value():
+            result.add_improvement(solution.value(), (datetime.now() - cpu_time).seconds)
+            result.add_best(solution)
+    return returning_value
 
 
 def get_best_order(
@@ -31,19 +59,19 @@ def find_result(path_input: Path, path_output: Path) -> Tuple[float, float]:
     result = Result()
     random.seed(0)
 
-    patients_list: List[Tuple[List[Patient], float]] = []
-    for heuristic in HeuristicGenerator().get_heuristics():
-        patients_list.append(get_best_order(result, path_input, heuristic, cpu_time))
-
-    for _ in range(30):
-        optimization = EvolutionaryAlgorithm(patients_list)
-        patients_list = []
-        for child in optimization.get_population():
-            patients_list.append(get_best_order(result, path_input, PredefinedOrder(child), cpu_time))
-        patients_list.append((optimization.get_best_exemplar()))
+    patients_list: List[Tuple[List[Patient], float]] = run_parallel(
+        HeuristicGenerator().get_heuristics(), result, path_input, cpu_time
+    )
+    with tqdm(total=30) as progress_bar:
+        for _ in range(30):
+            optimization = EvolutionaryAlgorithm(patients_list)
+            algorithms_list = [PredefinedOrder(child) for child in optimization.get_population()]
+            patients_list = run_parallel(algorithms_list, result, path_input, cpu_time)
+            patients_list.append((optimization.get_best_exemplar()))
+            progress_bar.set_postfix_str(optimization.get_data_progress_bar())
+            progress_bar.update(1)
 
     assert result.best_sol is not None
-    # Visualizer(result.best_sol).visualize()
     with open(path_output, "w+") as f:
         f.write(str(result))
 
--- src/heuristics/optimizer.py
+++ src/heuristics/optimizer.py
@@ -1,5 +1,5 @@
 import random
-from typing import List, Tuple
+from typing import List, Set, Tuple
 
 from ..elements.patient import Patient
 
@@ -8,15 +8,15 @@ class EvolutionaryAlgorithm:
     def __init__(self, population: List[Tuple[List[Patient], float]]) -> None:
         self.patients_orders, self.fitness = [list(x) for x in zip(*population)]
         self.elite_index = max(range(len(self.fitness)), key=self.fitness.__getitem__)
-        print(f"fitness: {round(sum(self.fitness)/len(self.fitness), 2)}, max_finess: {self.fitness[self.elite_index]}")
-        # self.crossover_rate = 0.9
-        # self.mutation_rate = 0.7
+        self.crossover_rate = 0.9
+        self.mutation_rate = 0.1
+        self.tournament_size = 3
 
     def get_population(self) -> List[List[Patient]]:
         population: List[List[Patient]] = []
         for _ in range(1, len(self.patients_orders)):
             parent_1 = self.roulette_selection()
-            parent_2 = self.roulette_selection()
+            parent_2 = self.tournament_selection()
             child = self.crossover(parent_1, parent_2)
             population.append(self.mutate(child))
         return population
@@ -25,20 +25,67 @@ class EvolutionaryAlgorithm:
         idx = random.choices(population=list(range(len(self.patients_orders))), weights=self.fitness, k=1)[0]
         return self.patients_orders[idx]
 
+    def tournament_selection(self) -> List[Patient]:
+        tournament_contestants = random.sample(range(len(self.patients_orders)), self.tournament_size)
+        winner = max(tournament_contestants, key=lambda x: self.fitness[x])
+        return self.patients_orders[winner]
+
     def crossover(self, parent_1: List[Patient], parent_2: List[Patient]) -> List[Patient]:
-        # if random.random() > self.crossover_rate:
-        #     return parent_1
         crossover_point = random.randint(0, len(parent_1))
         remaining_patients = [patient for patient in parent_2 if patient not in parent_1[:crossover_point]]
         child = parent_1[:crossover_point] + remaining_patients
         return child
 
+    def uniform_crossover(self, parent_1: List[Patient], parent_2: List[Patient]) -> List[Patient]:
+        mask = [random.choice([True, False]) for _ in range(len(parent_1))]
+        child: List[Patient] = []
+        used: Set[Patient] = set()
+
+        p1, p2 = 0, 0
+
+        for m in mask:
+            if m:
+                p1 = self._find_next(used, parent_1, p1)
+                self._add_patient_to_child(parent_1, p1, used, child)
+                p1 += 1
+            else:
+                p2 = self._find_next(used, parent_2, p2)
+                self._add_patient_to_child(parent_2, p2, used, child)
+                p2 += 1
+
+        self._add_remaining(parent_1[p1:], used, child)
+        self._add_remaining(parent_2[p2:], used, child)
+
+        return child
+
+    def _find_next(self, used: Set[Patient], parent: List[Patient], pointer: int) -> int:
+        while pointer < len(parent) and parent[pointer] in used:
+            pointer += 1
+        return pointer
+
+    def _add_patient_to_child(self, parent: List[Patient], pointer: int, used: Set[Patient], child: List[Patient]):
+        if pointer < len(parent):
+            used.add(parent[pointer])
+            child.append(parent[pointer])
+
+    def _add_remaining(self, remaining_patients: List[Patient], used: Set[Patient], child: List[Patient]):
+        for patient in remaining_patients:
+            if patient not in used:
+                used.add(patient)
+                child.append(patient)
+
     def mutate(self, child: List[Patient]) -> List[Patient]:
-        # if random.random() > self.mutation_rate:
-        #     return child
+        if random.random() > self.mutation_rate:
+            return child
         idx_1, idx_2 = random.sample(range(len(child)), 2)
         child[idx_1], child[idx_2] = child[idx_2], child[idx_1]
         return child
 
     def get_best_exemplar(self) -> Tuple[List[Patient], float]:
         return (self.patients_orders[self.elite_index], self.fitness[self.elite_index])
+
+    def get_data_progress_bar(self) -> str:
+        return (
+            f"fitness: {round(sum(self.fitness) / len(self.fitness), 2)}, "
+            + f"max_fitness: {self.fitness[self.elite_index]}"
+        )
